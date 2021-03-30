150d2c963ab8b79a8bf61455cca1643de9c39f85
sasha15455 <sasha15452@gmail.com>
2021-03-30 20:50:04 +0300

    First commit with all files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ChessBoardTask.Controller;
+using ChessBoardTask.Logic;
+using ChessBoardTask.View;
+
+namespace ChessBoardTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Instruction"); //ToDo: Implement instruction
+                return;
+            }
+
+            ChessBoardController chessBoardStarter = new ChessBoardController();
+
+            (int width, int height) = chessBoardStarter.Start();
+
+            ChessBoard chessBoardInit = new ChessBoard(width,height);
+
+            ChessBoardViewer chessBoardInterface = new ChessBoardViewer(chessBoardInit);
+            chessBoardInterface.ShowFullBoard();
+            Console.ReadKey();
+        }
+    }
+}
